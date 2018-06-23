451b03f338e75868ae5f75098277c96d45a47b99
Nitin Patel <nitinpatel278@gmail.com>
2018-06-24 02:41:11 +0530

    up to trillion

--- wordNum.js
+++ wordNum.js
@@ -18,13 +18,26 @@ function wordNum(num) {
     }
 
     if(num < 1000) {
-        return oneTo19[Math.floor(num/100)] + ' hundred ' +  ((num % 100 !== 0)?wordNum(num % 100): '')
+        return oneTo19[Math.floor(num/100)] + ' hundred ' +  ((num % 100 !== 0)? wordNum(num % 100) : '')
     }
 
-    return 'Input should be less than 1000'
+    if(num < 1000000){
+        return wordNum(Math.floor(num/1000)) + ' thousand ' + ((num % 1000 !== 0)? wordNum(num % 1000) : '')
+    }
+
+    if(num < 1000000000) {
+        return wordNum(Math.floor(num/1000000)) + ' million ' + ((num % 1000000 !== 0)? wordNum(num % 1000000) : '')
+    }
+
+    if(num < 1000000000000) {
+        return wordNum(Math.floor(num/1000000000)) + ' billion ' + ((num % 1000000000 !== 0)? wordNum(num % 1000000000) : '')
+    }
+
+    return 'Input should be less than 1000000000000'
 }
 
 
-for(let i = 0; i < 1000; i++){
+for(let i = 0; i < 100000; i++){
     console.log(wordNum(i))
 }
+
